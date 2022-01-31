f7a4f366398377f7857a0f37b9ddf9a26e9c63c8
Bernardo <bernardofbbraga@gmail.com>
2022-01-31 16:06:58 -0300

    add comments to build.js

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const Compiler = require("./tools/webpack/Compiler.js");
 const serve = require("./tools/webpack/serve.js");
 const watcher = require("./tools/webpack/watcher.js");
+const open = require("open");
 
 const myArgs = process.argv.slice(2);
 const devMode = myArgs[0] === "dev";
@@ -16,14 +17,18 @@ console.log("Running in", devMode ? "dev" : "production", "mode");
 const compiler = new Compiler(SSRPath, devMode);
 
 const run = async () => {
+  // use webpack's node API to compile
   await compiler.run();
 
   if (devMode) {
+    // start a local server to host the files
     serve(webServerPath, port);
 
+    // watch for any file change to re-compile
+    // and update the browser tab via socket
     watcher(wsPort, compiler);
 
-    const open = require("open");
+    // open a new tab for the user
     open(`http://localhost:${port}`);
   }
 };
